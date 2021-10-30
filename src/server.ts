dacab1dbcfff0d02e30d05232ea7638e7c383570
Jhonata Nascimento <87243089+jhonatanascimento@users.noreply.github.com>
2021-10-30 17:59:50 -0300

    Adicionado funções de BD

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import express, { response } from "express";
+import "reflect-metadata";
+import express from "express";
 
+import "./database";
 
 //@types/express -D
 const app = express();
@@ -8,22 +10,22 @@ const app = express();
 // GET - buscar uma informação
 // POST - enviar uma informação, inserir no banco
 // Put- alterar uma informação enviada por post
-// Delete- remover informação  
+// Delete- remover informação
 // Patch- alterar uma informação especifica
 
-
-
-
+/**
+ * Tipos de parametros
+ * Routes Params=> http://localhost:3000/produtos/ são parametros usados nas rotas
+ * Querie Params => São parametros passados na rota que filtram "/produtos?name=teclado&description=..."
+ *
+ * Body Params=>Parametros que vem no corpo da requisição, "name :teclado", usa no POST
+ */
 
 app.get("/test", (req, res) => {
-   //Request - tudo que está entrando
-   //response - tudo que está saindo
-   return res.send("Olá NLW"); 
-})
-
-app.post("/test-post", (req,res) => {
-   return res.send("Olá nlw metodo post")
-})
+  //Request - tudo que está entrando
+  //response - tudo que está saindo
+  return res.send("Olá NLW");
+});
 
 //http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
