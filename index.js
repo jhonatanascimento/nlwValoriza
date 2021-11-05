9065a1c19a249d5a4d444282350dabdb2d2dba40
jhonatanascimento <jhonata_ti@outlook.com>
2021-11-05 09:01:04 -0300

    problems with migrations fixed

--- index.js
+++
@@ -1,4 +0,0 @@
-function enviarEmail(nome, email) {
-    console.log("enviar email para " + nome + " com email " + email);
-}
-enviarEmail("Jhonata", 'jhony12@hotmail.com');
--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
-import "reflect-metadata";
-import express from "express";
-
-import "./database";
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+var express_1 = __importDefault(require("express"));
+require("./database");
 //@types/express -D
-const app = express();
-
+var app = (0, express_1.default)();
 // Methods mais comuns:
 // GET - buscar uma informação
 // POST - enviar uma informação, inserir no banco
 // Put- alterar uma informação enviada por post
 // Delete- remover informação
 // Patch- alterar uma informação especifica
-
 /**
  * Tipos de parametros
  * Routes Params=> http://localhost:3000/produtos/ são parametros usados nas rotas
@@ -20,6 +21,5 @@ const app = express();
  *
  * Body Params=>Parametros que vem no corpo da requisição, "name :teclado", usa no POST
  */
-
 //http://localhost:3000
-app.listen(3000, () => console.log("Server is running"));
+app.listen(3000, function () { return console.log("Server is running"); });
